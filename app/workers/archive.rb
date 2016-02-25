2fbbf0842e1f988f134d9783e6eb69dca9e6d81d
tranhuyhoangbka <hoangth92.nd@gmail.com>
2016-02-25 17:03:03 +0700

    ok next

--- app/workers/archive.rb
+++ app/workers/archive.rb
@@ -1,9 +1,15 @@
 class Archive
+  before_enqueue :send_message
   @queue = :foo
 
   class << self
     def perform
       puts "Now is: #{Time.now}\n"
     end
+  end
+
+  private
+  def send_message
+    puts "Hooks in resque"
   end
 end
